591c182292aa39e1722391a5444a4839d55d08fa
Andreas Kugel <andreas.kugel@web.de>
2025-05-24 11:41:22 +0200

    update

--- parseVoc.py
+++ parseVoc.py
@@ -6,17 +6,18 @@ from PIL import Image
 import json
 
 import matplotlib
+
 matplotlib.use("Agg")  # Use Agg backend (no GUI window)
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 # --- Label Mapping ---
-label_map = {'bike': 0, 'car': 1, 'person': 2}
+label_map = {"bike": 0, "car": 1, "person": 2}
 class_names = list(label_map.keys())
 # cell names is start x, start y, size
-cell_names = [f"{x:01d}_{y:01d}_1" for y  in range(0,3) for x in range(0,3)]
-cell_names += [f"{x:01d}_{y:01d}_2" for y  in range(0,2) for x in range(0,2)]
-cell_names += [f"{x:01d}_{y:01d}_3" for y  in range(0,1) for x in range(0,1)]
+cell_names = [f"{x:01d}_{y:01d}_1" for y in range(0, 3) for x in range(0, 3)]
+cell_names += [f"{x:01d}_{y:01d}_2" for y in range(0, 2) for x in range(0, 2)]
+cell_names += [f"{x:01d}_{y:01d}_3" for y in range(0, 1) for x in range(0, 1)]
 
 imgSize = 160
 
@@ -50,7 +51,7 @@ def parse_tugraz_annotation(txt_path, label_map, tugraz_root):
                 current_label = match.group(2)
                 # print(f"Found label: {current_label}")
                 if current_label == "none":
-                    break # empty image
+                    break  # empty image
         elif "Bounding box for object" in line and current_label:
             coords = re.findall(r"\((\d+),\s*(\d+)\)", line)
             # print(f"Found coordinates: {coords} for label: {current_label}")
@@ -59,11 +60,22 @@ def parse_tugraz_annotation(txt_path, label_map, tugraz_root):
                 xmax, ymax = map(int, coords[1])
                 xmin, xmax = xmin / orig_width, xmax / orig_width
                 ymin, ymax = ymin / orig_height, ymax / orig_height
-                bboxes.append([int(ymin*imgSize), int(xmin*imgSize), int(ymax*imgSize), int(xmax*imgSize)])
+                bboxes.append(
+                    [
+                        int(ymin * imgSize),
+                        int(xmin * imgSize),
+                        int(ymax * imgSize),
+                        int(xmax * imgSize),
+                    ]
+                )
                 labels.append(label_map[current_label])
                 current_label = None
 
-    return img_name, np.array(img) / 255.0, (np.array(bboxes, dtype=np.int32), np.array(labels, dtype=np.int32))
+    return (
+        img_name,
+        np.array(img) / 255.0,
+        (np.array(bboxes, dtype=np.int32), np.array(labels, dtype=np.int32)),
+    )
 
 
 # --- Dataset Loader ---
@@ -72,7 +84,8 @@ def load_dataset(root_dir):
     txt_files = [
         os.path.join(dp, f)
         for dp, dn, filenames in os.walk(ann_root)
-        for f in filenames if f.endswith(".txt")
+        for f in filenames
+        if f.endswith(".txt")
     ]
 
     if not txt_files:
@@ -86,7 +99,6 @@ def load_dataset(root_dir):
         except Exception as e:
             print(f"[WARN] Skipping {txt}: {e}")
 
-
     return annotated_imgs
 
 
@@ -97,8 +109,17 @@ def main():
     outDir = "./output"
     if not os.path.exists(outDir):
         os.makedirs(outDir)
+    else:
+        for file in os.listdir(outDir):
+            file_path = os.path.join(outDir, file)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
     print(f"Output directory: {outDir}")
-    
 
     for i, (name, img, (bboxes, labels)) in enumerate(sources):
         if i < 10:
@@ -106,38 +127,42 @@ def main():
             ax.imshow(img)
             for bbox, label in zip(bboxes, labels):
                 ymin, xmin, ymax, xmax = bbox
-                rect = patches.Rectangle((xmin, ymin),
-                                        (xmax - xmin),
-                                        (ymax - ymin),
-                                        linewidth=2, edgecolor='r', facecolor='none')
+                rect = patches.Rectangle(
+                    (xmin, ymin),
+                    (xmax - xmin),
+                    (ymax - ymin),
+                    linewidth=2,
+                    edgecolor="r",
+                    facecolor="none",
+                )
                 ax.add_patch(rect)
-                ax.text(xmin, ymin, class_names[label], color='white', fontsize=12)
-            plt.axis('off')
-            plt.savefig(f"output_{i}.png", bbox_inches='tight', pad_inches=0)
+                ax.text(xmin, ymin, class_names[label], color="white", fontsize=12)
+            plt.axis("off")
+            plt.savefig(f"output_{i}.png", bbox_inches="tight", pad_inches=0)
             plt.close(fig)
             print(f"Processed image {i + 1}, {name}")
             print(f"Image shape: {img.shape}")
             print(f"Bounding boxes: {bboxes}")
             print(f"Labels: {labels}")
             print("===" * 10)
-            print(f"\n") 
+            print(f"\n")
         # Save the image
         img_save_path = os.path.join(outDir, name)
         img = Image.fromarray((img * 255).astype(np.uint8))
         img.save(img_save_path)
-        print(f"Saved image to {img_save_path}")
         # Save the labels
         label_save_path = os.path.join(outDir, f"{name.split('.')[0]}_labels.json")
-        with open(label_save_path, 'w') as f:
-            json.dump({"img":name, "bboxes": bboxes.tolist(), "labels": labels.tolist()}, f)
-        print(f"Saved labels to {label_save_path}")
-        print("=" * 40)
+        with open(label_save_path, "w") as f:
+            json.dump(
+                {"img": name, "bboxes": bboxes.tolist(), "labels": labels.tolist()}, f
+            )
 
     # Save the label names map
     label_map_save_path = os.path.join(outDir, "label_map.json")
-    with open(label_map_save_path, 'w') as f:
+    with open(label_map_save_path, "w") as f:
         json.dump(label_map, f)
     print(f"Saved label map to {label_map_save_path}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
