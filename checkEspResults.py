9e4c02c55013ae7b886d73d148157a0a400b5731
Andreas Kugel <andreas.kugel@web.de>
2025-06-04 12:26:13 +0200

    check esp

---
+++ checkEspResults.py
@@ -0,0 +1,30 @@
+import layoutUtils as lu
+import json
+with open("espyolo/main/robot_204_labels.json") as f:
+    boxes = json.load(f)
+
+grid = 5
+imgSize = 160
+cells = lu.define_cells(imgSize,grid)
+regions = lu.define_regions(grid)
+numClasses = 5
+
+results = []
+for i,bb in enumerate(boxes["bboxes"]):
+    c0 = lu.find_cell(cells,bb[0],bb[1])
+    c1 = lu.find_cell(cells,bb[2],bb[3])
+    r = lu.find_region(regions,c0,c1)
+    cls = boxes["labels"][i]
+    print("BBox:", bb, "Class:",cls, "Cell:", c0, c1, "Region:", r, "VectorItem:", r * numClasses + cls)
+    segment = cls * len(regions)
+    print("Segment:", segment + r)
+    for c in range(numClasses):
+       print(f"Segment2 ({c}):", c * len(regions) + r)
+        
+
+esp = [6,43,109,707,723,725,778,805]
+for e in esp:
+    print("ESP:", e, "Class:", e % numClasses, "Region:", e // numClasses)
+for e in esp:
+    print("ESP2:", e, "Region:", e % len(regions), "Class:", e // len(regions))
+    
