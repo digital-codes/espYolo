fe19788ba9183eb4bdcf1e28525e988fd81a6eaf
Andreas Kugel <andreas.kugel@web.de>
2025-06-06 11:18:04 +0200

    update camera example

--- camEx/esp2.py
+++ camEx/esp2.py
@@ -1,10 +1,8 @@
 # client.py
 
-import socket
 from PIL import Image
 import struct
-import os 
-os.makedirs('img', exist_ok=True)
+
 
 def rgb565_to_rgb888(value):
     """Convert a single 16-bit RGB565 value to 24-bit RGB888 tuple."""
@@ -33,27 +31,11 @@ def create_image_from_rgb565(data, width, height):
     return img
 
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-soc.connect(("192.168.4.1", 3333))
 
-icnt = 0
 
-while True:
-    result_bytes = soc.recv(4) # the number means how the response can be in bytes
-    print("RAW Length:",result_bytes)
-    flen = int.from_bytes(result_bytes, 'big')
-    print("Length:",flen)
+# Example usage
+with open('img.raw', 'rb') as f:
+    raw_data = f.read()
 
-    result_bytes = bytearray()
-    while len(result_bytes) < flen:
-        chunk = soc.recv(flen - len(result_bytes))
-        if not chunk:
-            raise ConnectionError("Connection lost while receiving data")
-        result_bytes.extend(chunk)
-
-    imgrgb = create_image_from_rgb565(result_bytes,176,144)
-    imgrgb.save(f'img/img_{icnt:04d}.png')
-    icnt += 1
-    print(f"Image saved as img/img_{icnt:04d}.png")
-    # imgrgb.show()
-
+imgrgb = create_image_from_rgb565(raw_data,176,144)
+imgrgb.show()
--- camEx/main/take_picture.c
+++ camEx/main/take_picture.c
@@ -54,29 +54,6 @@
 
 #define BOARD_ESP32S3RCAM
 
-// WROVER-KIT PIN Map
-#ifdef BOARD_WROVER_KIT
-
-#define CAM_PIN_PWDN -1  //power down is not used
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 21
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 19
-#define CAM_PIN_D2 18
-#define CAM_PIN_D1 5
-#define CAM_PIN_D0 4
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#endif
-
 #ifdef BOARD_ESP32S3RCAM
 #define CAM_PIN_PWDN  -1
 #define CAM_PIN_RESET -1
@@ -98,51 +75,8 @@
 #define POWER_GPIO_NUM 18
 #endif 
 
-
-// ESP32Cam (AiThinker) PIN Map
-#ifdef BOARD_ESP32CAM_AITHINKER
-
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#endif
-// ESP32S3 (WROOM) PIN Map
-#ifdef BOARD_ESP32S3_WROOM
-#define CAM_PIN_PWDN 38
-#define CAM_PIN_RESET -1   //software reset will be performed
-#define CAM_PIN_VSYNC 6
-#define CAM_PIN_HREF 7
-#define CAM_PIN_PCLK 13
-#define CAM_PIN_XCLK 15
-#define CAM_PIN_SIOD 4
-#define CAM_PIN_SIOC 5
-#define CAM_PIN_D0 11
-#define CAM_PIN_D1 9
-#define CAM_PIN_D2 8
-#define CAM_PIN_D3 10
-#define CAM_PIN_D4 12
-#define CAM_PIN_D5 18
-#define CAM_PIN_D6 17
-#define CAM_PIN_D7 16
-#endif
 static const char *TAG = "example:take_picture";
 
-#if ESP_CAMERA_SUPPORTED
 static camera_config_t camera_config = {
     .pin_pwdn = CAM_PIN_PWDN,
     .pin_reset = CAM_PIN_RESET,
@@ -180,7 +114,7 @@ static esp_err_t init_camera(void)
 {
     //initialize the camera
     ESP_LOGI(TAG, "Cam init...");
-    #ifdef BOARD_ESP32S3RCAM
+
     ESP_LOGI(TAG, "ESP32S3RCAM...");
     gpio_config_t io_conf = {
         .pin_bit_mask = 1ULL << POWER_GPIO_NUM,
@@ -196,7 +130,6 @@ static esp_err_t init_camera(void)
     gpio_set_level(POWER_GPIO_NUM, 0);
     vTaskDelay(1000 / portTICK_RATE_MS);
 
-    #endif 
 
     esp_err_t err = esp_camera_init(&camera_config);
     if (err != ESP_OK)
@@ -207,18 +140,15 @@ static esp_err_t init_camera(void)
 
     return ESP_OK;
 }
-#endif
 
 void do_wifi(void *pvParameters);
 void image_socket_server_task(void *pvParameters);
-
 
 void app_main(void)
 {
     xTaskCreate((TaskFunction_t)do_wifi, "WiFiTask", 4096, NULL, 5, NULL);
     ESP_LOGI(TAG, "Wifi initialized successfully");
 
-#if ESP_CAMERA_SUPPORTED
 /*
 https://github.com/espressif/esp32-camera/issues/314
 https://forum.arduino.cc/t/esp32-cam-ov2640-dark-picture/1202399
@@ -297,10 +227,6 @@ https://forum.arduino.cc/t/esp32-cam-ov2640-dark-picture/1202399
         #endif
         vTaskDelay(grabDelayMs / portTICK_RATE_MS);
     }
-#else
-    ESP_LOGE(TAG, "Camera support is not available for this chip");
-    return;
-#endif
 }
 
 
